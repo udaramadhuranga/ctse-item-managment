fa514b9ae92f54758124a0778e4c3ca99c4b01eb
udara <udara@gl.block-stars.com>
2022-05-15 12:45:25 +0530

    item backend microservice developed

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+app.use(cors());
+
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
+const URL = process.env.MONGODB_URL;
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+const productRouter = require("./routes/productRoutes");
+
+mongoose.connect(URL).catch(console.error);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Mongo DB Connection success");
+});
+
+app.use("/product", productRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
